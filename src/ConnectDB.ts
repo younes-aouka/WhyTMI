1d906ddc9eff007d69d316a4e85d3c0cbea9f3a9
Younes-aouka <youneseaouka@gmail.com>
2025-09-23 12:35:44 +0100

    title and description metadata + favicon.ico

--- src/ConnectDB.ts
+++ src/ConnectDB.ts
@@ -1,7 +1,17 @@
 import pgPromise from "pg-promise";
 
+// dev mode
+// const pgp = pgPromise();
+// const db = pgp({
+//   host: "localhost",
+//   port: 5432,
+//   database: process.env.Database_Name,
+//   user: process.env.User_Name,
+//   password: process.env.Database_Password,
+// });
+// export { db };
+
+// production mode
 const pgp = pgPromise({});
 const db = pgp(process.env.DATABASE_URL!);
-
-// Export db instance
 export { db,pgp };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,16 @@ import { checkSignInNonMiddlware } from "@/utils/checkSignInNonMiddlware";
 import { Toaster } from "sonner";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+  title: 'WhyTMI',
+  description:'WhyTMI helps you understand complex problems with clear explanations and community feedback. Search, share, and solve together.',
+  icons:{
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico"
+  }
+}
 
 export default async function RootLayout({children,}:{children: React.ReactNode;}) {
   const session = (await cookies()).get('session');
