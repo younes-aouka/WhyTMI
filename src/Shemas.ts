92fe0daf15b05a007aa9192c33ed943556b0fae6
Younes-aouka <youneseaouka@gmail.com>
2025-09-30 11:07:52 +0100

    small fix

--- src/Shemas.ts
+++ src/Shemas.ts
@@ -10,7 +10,7 @@ export type SignInFormValues = z.infer<typeof SignInFormShema>;
 
 export const SignUpFormShema = z.object({
   fullName: z.string().min(4,{message:'your full_name must be at least 4 characters !'})
-  .max(15,{message:'your full_name lenght must not pass 15 characters'}).trim(),
+  .max(50,{message:'your full_name lenght must not pass 50 characters'}).trim(),
   email: z.email({message:'please enter a valid email !'}).trim(),
   password: z.string().min(4,{message: 'please enter a valid password !(at least 4 characters)'})
   .max(7,{message:'password should not pass 7 characters !'}).trim(),
@@ -40,7 +40,7 @@ export type NewPasswordFormValues = z.infer<typeof NewPasswordFormShema>;
 
 export const ProfileFormShema = z.object({
   fullName: z.string().min(4,{message:'your full_name must be at least 4 characters !'}).trim()
-  .max(15,{message:'your full_name lenght must not pass 15 characters'}),
+  .max(50,{message:'your full_name lenght must not pass 15 characters'}),
   email: z.email({message:'please enter a valid email !'}),
   password: z.string().min(4,{message: 'please enter a valid password !(at least 4 characters)'})
   .max(7,{message:'password should not pass 7 characters !'}).trim(),
@@ -49,7 +49,7 @@ export type ProfileFormValues = z.infer<typeof ProfileFormShema>;
 
 
 export const NewPostFormShema = z.object({
-  title: z.string().min(3,{message:'please enter a valid title!(between 3 and 50 characters)'}).max(150).trim() ,
+  title: z.string().min(3,{message:'please enter a valid title!(between 3 and 200 characters)'}).max(200).trim() ,
   categorie: z.string().nonempty('please choose a categorie!'),
   post: z.string().trim().min(3,{message:'your post should be at least 3 characters'})
   .max(200,{message:'your post shouldn\'t pass 200 characters!'})
@@ -58,7 +58,7 @@ export type NewPostFormValues = z.infer<typeof NewPostFormShema>;
 
 
 export const UpdatePostFormShema = z.object({
-  title: z.string().min(3,{message:'please enter a valid title!(between 3 and 50 characters)'}).max(150).trim() ,
+  title: z.string().min(3,{message:'please enter a valid title!(between 3 and 200 characters)'}).max(200).trim() ,
   categorie: z.string().nonempty('please choose a categorie!'),
   post: z.string().trim().min(3,{message:'your post should be at least 3 characters'})
   .max(200,{message:'your post shouldn\'t pass 200 characters!'}),
